0169dc9d77ab21418b182b61e85389161e7c2849
Anton <dasantonym@users.noreply.github.com>
2020-02-29 11:22:21 +0100

    use https

--- download_models.sh
+++ download_models.sh
@@ -1,4 +1,4 @@
 echo "downloading inception weights"
-curl "http://hyper.fail/neuralart-models/inception_caffe.th" -o models/inception_caffe.th
+curl "https://hyper.fail/neuralart-models/inception_caffe.th" -o models/inception_caffe.th
 echo "downloading vgg weights"
-curl "http://hyper.fail/neuralart-models/vgg_normalized.th" -o models/vgg_normalized.th
+curl "https://hyper.fail/neuralart-models/vgg_normalized.th" -o models/vgg_normalized.th
